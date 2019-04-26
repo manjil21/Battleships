756f0bb33c7f7ddb1292e9649730dbb12eb1161a
azat22 <azatullah11@hotmail.com>
2019-04-26 13:01:20 +1000

    Changed SwinGame intro, removed some unnecessary code
    I removed the old swingame intro and added the new one. I removed some unnecessary code.

--- battships-C#/src/GameState.cs
+++ battships-C#/src/GameState.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 //using System.Data;
 using System.Diagnostics;
+
 /// <summary>
 /// The GameStates represent the state of the Battleships game play.
 /// This is used to control the actions and view displayed to
